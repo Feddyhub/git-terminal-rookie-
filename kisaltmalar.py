c03cb8fc0bcb5725fba8c194df6018218a8cc932
Murat Ferhat Derya <91869273+Feddyhub@users.noreply.github.com>
2025-03-16 03:55:09 +0300

    bitti

--- kisaltmalar.py
+++
@@ -1,5 +0,0 @@
-print("sirasiyla git add. : git status te (modifiye edilmisler)hepsini secer")
-print("git commit : bu dosyalari kendi localinde gunceller")
-print("git push : bunlari cloud warehouse'a yani gitHub'a tasir/gunceller")
-
-print("Bu islemleri terminalde yapmak kolaydir")
