2d7ad940bef7ae1aebca3721109f0a15e545e5b3
Murat Ferhat Derya <strangemfeddy@gmail.com>
2025-03-16 03:47:25 +0300

    degisiklik

--- kisaltmalar.py
+++ kisaltmalar.py
@@ -1,34 +1,5 @@
-import requests
-import pandas as pd
+print("sirasiyla git add. : git status te (modifiye edilmisler)hepsini secer")
+print("git commit : bu dosyalari kendi localinde gunceller")
+print("git push : bunlari cloud warehouse'a yani gitHub'a tasir/gunceller")
 
-API_URL = "https://api.coincap.io/v2/assets"
-
-def fetch_crypto_data():
-    response = requests.get(API_URL)
-    
-    if response.status_code == 200:
-        data = response.json().get('data', [])  # API’den gelen 'data' listesini al
-        return data
-    else:
-        print(f"API Hatası: {response.status_code}")
-        return []
-
-def save_to_excel(crypto_data):
-    # DataFrame oluştur
-    df = pd.DataFrame(crypto_data)
-    
-    # Excel dosyasına kaydet
-    df.to_excel("crypto_data.xlsx", index=False)
-    print("Veriler başarıyla 'crypto_data.xlsx' dosyasına kaydedildi.")
-    print("aga karisti")
-def main():
-    crypto_data = fetch_crypto_data()
-    #git icin deneme
-    # 'search': crypto_symbol şeklinde her bir kripto varlığını listele
-    formatted_data = [{"search": asset.get("symbol", ""), "name": asset.get("name", "")} for asset in crypto_data]
-    
-    save_to_excel(formatted_data)
-
-  
-if __name__ == "__main__":
-    main()
+print("Bu islemleri terminalde yapmak kolaydir")
